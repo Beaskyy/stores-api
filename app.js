938a6a80ba6c00e5147fec2f63c9f4871ad3c0eb
Beaskyy <afeezakintobi@nqb8.co>
2023-12-05 20:52:39 +0100

    create port

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const ErrorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store api</h1> <a href="/api/v1/products">products routes</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(ErrorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () =>{
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server listening to port ${port}...`));
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
